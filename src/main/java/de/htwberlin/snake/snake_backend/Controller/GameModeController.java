1d07a5b0c4d4098f94625359647add9e0c4f91c6
Atakan Yalcin <Atakan.Yalcin@Student.HTW-Berlin.de>
2025-01-15 13:07:12 +0100

    CORS Annotation

--- src/main/java/de/htwberlin/snake/snake_backend/Controller/GameModeController.java
+++ src/main/java/de/htwberlin/snake/snake_backend/Controller/GameModeController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "https://snake-frontend-mlix.onrender.com")
 @RestController
 @RequestMapping("/api/gamemodes")
 public class GameModeController {
